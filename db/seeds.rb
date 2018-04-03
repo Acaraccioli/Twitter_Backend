eb1f9bbabf156d2cf74fab5bc5a6f7a89f5918ea
Acaraccioli <alejandro-42@hotmail.com>
2018-04-02 18:18:07 -0600

    primero

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+Tweets = Tweet.create(
+  [
+    {
+      
+      body: "Made of chocolate",
+      user:"u1"
+    },
+    {
+      body: "Only for replying to mentions and DMs",
+      user:"u2"
+    },
+    {
+      body: "A mafia crime drama starring Berlusconi",
+      user:"u3"
+    },
+    {
+      body: "Like Uno but involves drinking",
+      user:"u3"
+    }
+  ])
